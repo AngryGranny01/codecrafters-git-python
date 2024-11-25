5c878c31657f81ef10209d241855f19781e82979
rodie01 <rojo.dienst@gmail.com>
2024-11-25 21:15:10 +0100

    Test write Tree

--- app/main.py
+++ app/main.py
@@ -130,7 +130,8 @@ def write_tree_handler(directory):
             compressed_blob = hashlib.sha1(uncompressed_blob).hexdigest()
             tree_entries.append(compressed_blob)
         elif os.path.isdir(entry_path):
-            recursive_tree_hash_generation(entry_path)
+            if entry_path != "./git":
+                recursive_tree_hash_generation(entry_path)
         else:
             continue # Skip unsupported entries
     return
