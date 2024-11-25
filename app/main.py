171fd5a9cb21d8c7a048b9be01076ef0fa0760e7
rodie01 <rojo.dienst@gmail.com>
2024-11-25 17:25:58 +0100

    Test blub

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main():
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
-        os.mkdir(directory_objects_path)
+        os.mkdir(".git/objects")
         os.mkdir(".git/refs")
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
