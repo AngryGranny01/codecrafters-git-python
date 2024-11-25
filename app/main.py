736aae239ee8e1b5556560f153367b73aa464015
rodie01 <rojo.dienst@gmail.com>
2024-11-25 17:16:50 +0100

    Test blub

--- app/main.py
+++ app/main.py
@@ -1,18 +1,25 @@
 import sys
 import os
+import zlib
 
+directory_objects_path = ".git/objects"
 
 def main():
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
-        os.mkdir(".git/objects")
+        os.mkdir(directory_objects_path)
         os.mkdir(".git/refs")
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
     else:
         raise RuntimeError(f"Unknown command #{command}")
+
+    for path, directories, files in os.walk(directory_objects_path):
+        if file in files:
+             print('found %s' % os.path.join(path, file))
+    output = sys.stdout
 
 
 if __name__ == "__main__":
