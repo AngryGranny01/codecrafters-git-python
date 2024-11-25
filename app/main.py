cf4aff85965ec31c69d394b9ea19e48c0f7d6922
rodie01 <rojo.dienst@gmail.com>
2024-11-25 18:00:21 +0100

    Test write Blub

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ def object_read():
 def object_write(content_path):
     with open(content_path, "rt") as f:
         newcontent = f.read()
-        uncompressed_content = b'blob ' + str(len(newcontent)).encode() + b'\x00' + newcontent
+        uncompressed_content = b'blob ' + str(len(newcontent)).encode() + b'\x00' + bytes(newcontent)
         print(uncompressed_content)
         compressed_content = zlib.compress(uncompressed_content)
         print(compressed_content)
