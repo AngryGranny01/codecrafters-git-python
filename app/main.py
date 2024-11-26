de456f14ed6665e265b614798384c52082eadca7
rodie01 <rojo.dienst@gmail.com>
2024-11-26 14:29:15 +0100

    Test commit Tree

--- app/main.py
+++ app/main.py
@@ -232,7 +232,7 @@ def handle_commit_tree():
 
 def create_commit_tree(parent_file,parent_tree_sha, commit_sha, message):
     # get author 
-    print("Parent File: "+parent_file)
+    print(parent_file)
     print("Tree Sha: "+ parent_tree_sha)
     print(commit_sha)
     print(message)
